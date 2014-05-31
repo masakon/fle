5149a9a201bcb2d027370155f5f42ffe7c2af3dc
Thierry Bertho <thierry.bertho@gmail.com>
2014-05-31 18:13:27 +0200

    Test Phpstorm

--- index.php
+++ index.php
@@ -8,9 +8,7 @@ echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.or
 	<title>FLE-API : 日本人へのフランス語の勉強のツール</title>
 	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
 	<meta http-equiv="pragma" content="no-cache" /> 
-	<meta http-equiv="cache-control" content="no-cache, must-revalidate" /> 
-	<style type="text/css">
-	<!-- body {font-family:Consolas; font-size: 20px; } -->
+	<meta http-equiv="cache-control" content="no-cache, must-revalidate" />
 </style>
 	
 	</head>
